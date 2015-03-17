9ceda45c62db34279bf70c7ea5bbc6e3acd8c1fc
flawwan <flawwan@gmail.com>
2015-03-17 08:49:30 +0100

    Renamed some files

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="js/script.js"></script>
+<script src="js/chat-turn-example.js"></script>
 <script>
 	api.match = <?=$match;?>;
 	api.ajax();
